8b596d064ba25ea87f4770ee0a9d7da4aecd6fff
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-09-20 16:30:45 -0500

    Citizen Class & Citizens

---
+++ Citizens/Egiptian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyP2_ExamenIndividual1
+{
+    public class Egiptian : Citizen
+    {
+        private const int FARMING_POINTS = 1;
+        private const int FISHING_POINTS = 4;
+        private const int HARVESTING_POINTS = 2;
+        private const int MINING_POINTS = 3;
+
+        public Egiptian() : base(FARMING_POINTS, FISHING_POINTS, HARVESTING_POINTS, MINING_POINTS) { }     
+    }
+}
+
